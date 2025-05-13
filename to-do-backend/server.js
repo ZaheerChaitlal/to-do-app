e917e0ec777c5d679bfcf1278441f44a24504511
ZaheerChaitlal <zaheerchaitlal@gmail.com>
2025-05-13 19:34:50 -0400

    Updated CORS settings

--- to-do-backend/server.js
+++ to-do-backend/server.js
@@ -9,7 +9,9 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
     origin: process.env.CLIENT_URL || 'http://localhost:3000',
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
+
 app.use(cors(corsOptions));
 app.use(express.json());
 
